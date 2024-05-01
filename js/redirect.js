c6d8eeed352ef1f80374211f1b8981b8cf421fc7
Nate <121198197+Dabbie50@users.noreply.github.com>
2024-04-30 21:15:51 -0600

    Update #4

---
+++ js/redirect.js
@@ -0,0 +1,33 @@
+/*
+curl -X POST "https://accounts.spotify.com/api/token" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -d "grant_type=client_credentials&client_id=your-client-id&client_secret=your-client-secret"
+*/
+
+const urlParams = new URLSearchParams(window.location.search);
+let code = urlParams.get('code');
+
+const getToken = async code => {
+
+  // stored in the previous step
+  let codeVerifier = localStorage.getItem('code_verifier');
+
+  const payload = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      client_id: clientId,
+      grant_type: 'authorization_code',
+      code,
+      redirect_uri: redirectUri,
+      code_verifier: codeVerifier,
+    }),
+  }
+
+  const body = await fetch(url, payload);
+  const response =await body.json();
+
+  localStorage.setItem('access_token', response.access_token);
+}
--- js/tmp.mjs
+++ js/tmp.mjs
@@ -3,22 +3,69 @@ curl -X POST "https://accounts.spotify.com/api/token" \
      -H "Content-Type: application/x-www-form-urlencoded" \
      -d "grant_type=client_credentials&client_id=your-client-id&client_secret=your-client-secret"
 */
-var client_id = 'd42fa8b098a14a769be65e9d110f5646';
-var client_secret = '58add988e7f74c52b2f65d9939da3d0a';
+// var client_id = 'd42fa8b098a14a769be65e9d110f5646';
+// var client_secret = '58add988e7f74c52b2f65d9939da3d0a';
 
-var authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization': 'Basic ' + client_id + ":" + client_secret
-  },
-  form: {
-    grant_type: 'client_credentials'
-  },
-  json: true
-};
+// var authOptions = {
+//   url: 'https://accounts.spotify.com/api/token',
+//   headers: {
+//     'Authorization': 'Basic ' + client_id + ":" + client_secret
+//   },
+//   form: {
+//     grant_type: 'client_credentials'
+//   },
+//   json: true
+// };
 
-request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-    var token = body.access_token;
-  }
-});
+// request.post(authOptions, function(error, response, body) {
+//   if (!error && response.statusCode === 200) {
+//     var token = body.access_token;
+//   }
+// });
+
+
+const generateRandomString = (length) => {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const values = crypto.getRandomValues(new Uint8Array(length));
+  return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+}
+
+const codeVerifier  = generateRandomString(64);
+
+const sha256 = async (plain) => {
+  const encoder = new TextEncoder()
+  const data = encoder.encode(plain)
+  return window.crypto.subtle.digest('SHA-256', data)
+}
+
+const base64encode = (input) => {
+  return btoa(String.fromCharCode(...new Uint8Array(input)))
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+}
+
+const hashed = await sha256(codeVerifier)
+const codeChallenge = base64encode(hashed);
+
+
+const clientId = 'YOUR_CLIENT_ID';
+const redirectUri = 'dabbie50.github.io/redirect.html';
+
+const scope = 'user-read-private user-read-email';
+const authUrl = new URL("https://accounts.spotify.com/authorize")
+
+// generated in the previous step
+window.localStorage.setItem('code_verifier', codeVerifier);
+
+const params =  {
+  response_type: 'code',
+  client_id: clientId,
+  scope,
+  code_challenge_method: 'S256',
+  code_challenge: codeChallenge,
+  redirect_uri: redirectUri,
+}
+
+authUrl.search = new URLSearchParams(params).toString();
+window.location.href = authUrl.toString();
