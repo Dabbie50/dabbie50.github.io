9a828d832f0eebe52472a81e7bfbb63e7f788494
Nate <121198197+Dabbie50@users.noreply.github.com>
2024-08-09 22:39:14 +0000

    Update for contact info

--- js/tmp.mjs
+++ js/tmp.mjs
@@ -22,50 +22,52 @@ curl -X POST "https://accounts.spotify.com/api/token" \
 //     var token = body.access_token;
 //   }
 // });
-
-
-const generateRandomString = (length) => {
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const values = crypto.getRandomValues(new Uint8Array(length));
-  return values.reduce((acc, x) => acc + possible[x % possible.length], "");
-}
-
-const codeVerifier  = generateRandomString(64);
-
-const sha256 = function(plain) {
-  const encoder = new TextEncoder()
-  const data = encoder.encode(plain)
-  return window.crypto.subtle.digest('SHA-256', data)
-}
-
-const base64encode = (input) => {
-  return btoa(String.fromCharCode(...new Uint8Array(input)))
-    .replace(/=/g, '')
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_');
-}
-
-const hashed = sha256(codeVerifier);
-const codeChallenge = base64encode(hashed);
-
-
-const clientId = 'd42fa8b098a14a769be65e9d110f5646';
-const redirectUri = 'dabbie50.github.io/redirect.html';
-
-const scope = 'playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private';
-const authUrl = new URL("https://accounts.spotify.com/authorize")
-
-// generated in the previous step
-window.localStorage.setItem('code_verifier', codeVerifier);
-
-const params =  {
-  response_type: 'code',
-  client_id: clientId,
-  scope,
-  code_challenge_method: 'S256',
-  code_challenge: codeChallenge,
-  redirect_uri: redirectUri,
-}
-
-authUrl.search = new URLSearchParams(params).toString();
-window.location.href = authUrl.toString();
+let spotifyLink = document.getElementById("spotify")
+spotifyLink.addEventListener("click",{
+  const generateRandomString = (length) => {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const values = crypto.getRandomValues(new Uint8Array(length));
+    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+  }
+  
+  const codeVerifier  = generateRandomString(64);
+  
+  const sha256 = function(plain) {
+    const encoder = new TextEncoder()
+    const data = encoder.encode(plain)
+    return window.crypto.subtle.digest('SHA-256', data)
+  }
+  
+  const base64encode = (input) => {
+    return btoa(String.fromCharCode(...new Uint8Array(input)))
+      .replace(/=/g, '')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_');
+  }
+  
+  const hashed = sha256(codeVerifier);
+  const codeChallenge = base64encode(hashed);
+  
+  
+  const clientId = 'd42fa8b098a14a769be65e9d110f5646';
+  const redirectUri = 'dabbie50.github.io/redirect.html';
+  
+  const scope = 'playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private';
+  const authUrl = new URL("https://accounts.spotify.com/authorize")
+  
+  // generated in the previous step
+  window.localStorage.setItem('code_verifier', codeVerifier);
+  
+  const params =  {
+    response_type: 'code',
+    client_id: clientId,
+    scope,
+    code_challenge_method: 'S256',
+    code_challenge: codeChallenge,
+    redirect_uri: redirectUri,
+  }
+  
+  authUrl.search = new URLSearchParams(params).toString();
+  window.location.href = authUrl.toString();
+  
+})
